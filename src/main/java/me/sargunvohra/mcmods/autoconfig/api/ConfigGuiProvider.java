1dc791611935bf39603e4ff6f8685c25b1e5183d
Sargun Vohra <sargun.vohra@gmail.com>
2019-04-20 22:12:34 -0700

    fix build

--- src/main/java/me/sargunvohra/mcmods/autoconfig/api/ConfigGuiProvider.java
+++ src/main/java/me/sargunvohra/mcmods/autoconfig/api/ConfigGuiProvider.java
@@ -4,14 +4,12 @@ import me.shedaniel.cloth.gui.ClothConfigScreen;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
-import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.util.List;
 
 @FunctionalInterface
 @Environment(EnvType.CLIENT)
 public interface ConfigGuiProvider {
-    @Nullable
     List<ClothConfigScreen.AbstractListEntry> get(
         String i13n,
         Field field,
