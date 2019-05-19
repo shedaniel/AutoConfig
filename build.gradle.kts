642e94def25b3fa373d9f15d4d5495681a57d1d2
Sargun Vohra <sargun.vohra@gmail.com>
2019-05-18 21:01:00 -0700

    oops

--- build.gradle.kts
+++ build.gradle.kts
@@ -153,7 +153,6 @@ if (versionDetails().isCleanTag) {
             relations(closureOf<CurseRelation>{
                 requiredDependency("fabric")
                 requiredDependency("cloth-config")
-                requiredDependency("auto-config")
             })
         })
 
