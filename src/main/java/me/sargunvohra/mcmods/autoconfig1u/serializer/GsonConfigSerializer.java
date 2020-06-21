f86c46c067172c6ba2bdc428dcc4640f199de6c4
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-06-21 20:19:43 +0300

    Implement KeyCode properly

--- src/main/java/me/sargunvohra/mcmods/autoconfig1u/serializer/GsonConfigSerializer.java
+++ src/main/java/me/sargunvohra/mcmods/autoconfig1u/serializer/GsonConfigSerializer.java
@@ -1,16 +1,16 @@
 package me.sargunvohra.mcmods.autoconfig1u.serializer;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
 import me.sargunvohra.mcmods.autoconfig1u.util.Utils;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.util.InputUtil;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -32,7 +32,8 @@ public class GsonConfigSerializer<T extends ConfigData> implements ConfigSeriali
     }
 
     public GsonConfigSerializer(Config definition, Class<T> configClass) {
-        this(definition, configClass, new GsonBuilder().setPrettyPrinting().create());
+        this(definition, configClass, new GsonBuilder().registerTypeAdapter(InputUtil.KeyCode.class, new KeyCodeJsonAdapter())
+            .setPrettyPrinting().create());
     }
 
     private Path getConfigPath() {
@@ -72,5 +73,28 @@ public class GsonConfigSerializer<T extends ConfigData> implements ConfigSeriali
     @Override
     public T createDefault() {
         return Utils.constructUnsafely(configClass);
+    }
+
+    // I'm pretty sure Gson can serialize KeyCode just fine? but just in case...
+    // besides, this is technically an optimization - KeyCode also has a "name" field that's safe to ignore
+    // - ADudeCalledLeo
+    public static class KeyCodeJsonAdapter implements JsonSerializer<InputUtil.KeyCode>,
+                                                      JsonDeserializer<InputUtil.KeyCode> {
+        @Override
+        public JsonElement serialize(InputUtil.KeyCode src, Type srcT, JsonSerializationContext ctx) {
+            final JsonObject jObj = new JsonObject();
+            jObj.add("type", ctx.serialize(src.getCategory()));
+            jObj.add("code", ctx.serialize(src.getKeyCode(), Integer.class)); // why u converting ints to floats
+            return jObj;
+        }
+
+        @Override
+        public InputUtil.KeyCode deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext ctx)
+            throws JsonParseException {
+            final JsonObject jObj = json.getAsJsonObject();
+            final InputUtil.Type type = ctx.deserialize(jObj.get("type"), InputUtil.Type.class);
+            final int code = jObj.get("code").getAsInt();
+            return type.createFromCode(code);
+        }
     }
 }
--- src/main/java/me/sargunvohra/mcmods/autoconfig1u/serializer/JanksonConfigSerializer.java
+++ src/main/java/me/sargunvohra/mcmods/autoconfig1u/serializer/JanksonConfigSerializer.java
@@ -1,16 +1,22 @@
 package me.sargunvohra.mcmods.autoconfig1u.serializer;
 
 import blue.endless.jankson.Jankson;
+import blue.endless.jankson.JsonElement;
+import blue.endless.jankson.JsonObject;
+import blue.endless.jankson.impl.Marshaller;
 import blue.endless.jankson.impl.SyntaxError;
 import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
 import me.sargunvohra.mcmods.autoconfig1u.util.Utils;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.util.InputUtil;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * This serializer serializes configs into Json5 files using Jankson.
@@ -29,7 +35,11 @@ public class JanksonConfigSerializer<T extends ConfigData> implements ConfigSeri
     }
 
     public JanksonConfigSerializer(Config definition, Class<T> configClass) {
-        this(definition, configClass, Jankson.builder().build());
+        this(definition, configClass, Jankson.builder()
+            .registerTypeFactory(InputUtil.KeyCode.class, () -> InputUtil.UNKNOWN_KEYCODE)
+            .registerTypeAdapter(InputUtil.KeyCode.class, new KeyCodeJsonTypeAdapter())
+            .registerSerializer(InputUtil.KeyCode.class, new KeyCodeJsonSerializer())
+            .build());
     }
 
     private Path getConfigPath() {
@@ -66,5 +76,26 @@ public class JanksonConfigSerializer<T extends ConfigData> implements ConfigSeri
     @Override
     public T createDefault() {
         return Utils.constructUnsafely(configClass);
+    }
+
+    // not sure how Jankson handles (de)serialization errors exactly, I'll just assume a try/catch?
+    // hopefully this won't break horribly
+    // - ADudeCalledLeo
+    public static class KeyCodeJsonTypeAdapter implements Function<JsonObject, InputUtil.KeyCode> {
+        @Override
+        public InputUtil.KeyCode apply(JsonObject jsonObject) {
+            final InputUtil.Type type = jsonObject.get(InputUtil.Type.class, "type");
+            final int code = jsonObject.get(Integer.class, "code");
+            return type.createFromCode(code);
+        }
+    }
+    public static class KeyCodeJsonSerializer implements BiFunction<InputUtil.KeyCode, Marshaller, JsonElement> {
+        @Override
+        public JsonElement apply(InputUtil.KeyCode keyCode, Marshaller marshaller) {
+            final JsonObject jObj = new JsonObject();
+            jObj.put("type", marshaller.serialize(keyCode.getCategory()));
+            jObj.put("code", marshaller.serialize(keyCode.getKeyCode()));
+            return jObj;
+        }
     }
 }
