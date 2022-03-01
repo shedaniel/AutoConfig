1d42a539e96594a9d389820264e30ad2c08c4af4
shedaniel <daniel@shedaniel.me>
2022-03-01 13:05:27 +0800

    Update to 1.18.1
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- src/main/java/me/sargunvohra/mcmods/autoconfig1u/example/ExampleModMenuCompat.java
+++ src/main/java/me/sargunvohra/mcmods/autoconfig1u/example/ExampleModMenuCompat.java
@@ -1,7 +1,7 @@
 package me.sargunvohra.mcmods.autoconfig1u.example;
 
-import io.github.prospector.modmenu.api.ConfigScreenFactory;
-import io.github.prospector.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -9,11 +9,6 @@ import net.fabricmc.api.Environment;
 @SuppressWarnings("unused") // entrypoint
 @Environment(EnvType.CLIENT)
 public class ExampleModMenuCompat implements ModMenuApi {
-    @Override
-    public String getModId() {
-        return "autoconfig1u";
-    }
-
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return screen -> AutoConfig.getConfigScreen(ExampleConfig.class, screen).get();
